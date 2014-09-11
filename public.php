4d4c6f877ef708595822c32ada1bf2ff992dd9d4
Robin Appelman <icewind@owncloud.com>
2014-09-11 16:38:06 +0200

    Use incognito mode for public.php

--- public.php
+++ public.php
@@ -37,9 +37,9 @@ try {
 	OC_App::loadApps(array('authentication'));
 	OC_App::loadApps(array('filesystem', 'logging'));
 
-	OC_Util::checkAppEnabled($app, true);
-	OC_App::loadApp($app);
 	OC_User::setIncognitoMode(true);
+	OC_Util::checkAppEnabled($app);
+	OC_App::loadApp($app);
 
 	$baseuri = OC::$WEBROOT . '/public.php/' . $service . '/';
 
